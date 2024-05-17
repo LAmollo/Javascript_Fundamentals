abd69884aace7631c4c27d2eb6356a0eeafa28bc
LAmollo <lawianyamuga@gmail.com>
2024-05-17 00:47:48 -0700

    ###

--- scripts.mjs
+++ scripts.mjs
@@ -99,7 +99,48 @@ const AssignmentGroup = {
   ]
 };
 
-
+const LearnerSubmissions = [
+  {
+    learner_id: 125,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 47
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140
+    }
+  }
+];
 
 
 function calculateWeightedAverage(assignments, submissions) {
