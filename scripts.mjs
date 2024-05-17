16c12d4fdbb10968018ff6b1fc8bb2bcf1fd2adf
LAmollo <lawianyamuga@gmail.com>
2024-05-17 00:43:20 -0700

    var

--- scripts.mjs
+++ scripts.mjs
@@ -68,6 +68,40 @@
 //which should be an array of objects as described above.
 //You may use as many helper functions as you see fit.
 
+const CourseInfo = {
+  id: 451,
+  name: "Introduction to JavaScript"
+};
+const AssignmentGroup = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
+  assignments: [
+    {
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50
+    },
+    {
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500
+    }
+  ]
+};
+
+
+
+
 function calculateWeightedAverage(assignments, submissions) {
     let totalScore = 0;
     let totalWeight = 0;
